934a777983995e38c2aa010897b75514b7db34b1
TheOne <tanwei.rush@gmail.com>
2016-03-22 11:40:38 +0800

    配置文件修改，可以配置email

--- stock_warms.go
+++ stock_warms.go
@@ -9,8 +9,6 @@ import (
 	"strconv"
 	"strings"
 
-	"gopkg.in/redis.v3"
-
 	tanweiTools "github.com/sunreaver/goTools"
 	"github.com/sunreaver/mahonia"
 )
@@ -20,72 +18,77 @@ var (
 	StockDataRegexp = regexp.MustCompile(`="(.*)";`)
 )
 
+type Config struct {
+	Mail   []string `json:"emails"`
+	Stocks []string `json:"stocks"`
+}
+
 func main() {
-	stocks, e := readFile(tanweiTools.CurPath() + tanweiTools.SystemSep() + "stock.json")
+	configs, e := readFile(tanweiTools.CurPath() + tanweiTools.SystemSep() + "stock.json")
 	if e != nil {
 		fmt.Println(e.Error())
 		return
 	}
 
-	client := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
-	cmd := client.Set("key", "heheeh", 0)
-	if cmd.Err() {
-
-	}
-
 	format := "%s:\t>>>>>昨收:%s\t今收:%s\t今开:%s\t幅度:%s<<<<<\r\n"
-	outStr := ""
 
-	for _, v := range stocks {
-		resp, err := http.Get("http://hq.sinajs.cn/list=" + v)
-		if err != nil {
+	for _, cfg := range configs {
+
+		if len(cfg.Mail) == 0 {
 			continue
 		}
-		defer resp.Body.Close()
-		body, e := ioutil.ReadAll(resp.Body)
+		outStr := ""
+		for _, v := range cfg.Stocks {
+			resp, err := http.Get("http://hq.sinajs.cn/list=" + v)
+			if err != nil {
+				continue
+			}
+			defer resp.Body.Close()
+			body, e := ioutil.ReadAll(resp.Body)
+			if e != nil {
+				continue
+			}
+
+			str := string(body)
+			enc := mahonia.NewDecoder("gbk")
+			matchs := StockDataRegexp.FindAllStringSubmatch(enc.ConvertString(str), -1)
+			if len(matchs) == 0 || len(matchs[0]) < 2 {
+				continue
+			}
+
+			numerical := strings.Split(matchs[0][1], ",")
+			if len(numerical) < 5 {
+				continue
+			}
+
+			upDown := "未知"
+			yestoday, e1 := strconv.ParseFloat(numerical[2], 64)
+			today, e2 := strconv.ParseFloat(numerical[3], 64)
+			if e1 == nil && e2 == nil {
+				upDown = strconv.FormatFloat(today-yestoday, 'f', -1, 64)[0:6]
+			}
+			outStr = outStr + fmt.Sprintf(format, numerical[0], numerical[2], numerical[3], numerical[1], upDown)
+		}
+
+		outStr = outStr + "\r\nHappy day!\r\n"
+
+		e = tanweiTools.SendMail(outStr, cfg.Mail)
 		if e != nil {
-			continue
+			fmt.Println(e.Error())
 		}
-
-		str := string(body)
-		enc := mahonia.NewDecoder("gbk")
-		matchs := StockDataRegexp.FindAllStringSubmatch(enc.ConvertString(str), -1)
-		if len(matchs) == 0 || len(matchs[0]) < 2 {
-			continue
-		}
-
-		numerical := strings.Split(matchs[0][1], ",")
-		if len(numerical) < 5 {
-			continue
-		}
-
-		upDown := "未知"
-		yestoday, e1 := strconv.ParseFloat(numerical[2], 64)
-		today, e2 := strconv.ParseFloat(numerical[3], 64)
-		if e1 == nil && e2 == nil {
-			upDown = strconv.FormatFloat(today-yestoday, 'f', -1, 64)[0:6]
-		}
-		outStr = outStr + fmt.Sprintf(format, numerical[0], numerical[2], numerical[3], numerical[1], upDown)
-	}
-
-	outStr = outStr + "\r\nHappy day!\r\n"
-
-	e = tanweiTools.SendMail(outStr, []string{"tanwei.rush@gmail.com"})
-	if e != nil {
-		fmt.Println(e.Error())
-		fmt.Println(outStr)
 	}
 }
 
-func readFile(fileName string) ([]string, error) {
+func readFile(fileName string) ([]Config, error) {
 	b, e := ioutil.ReadFile(fileName)
 	if e != nil {
 		return nil, e
 	}
-	var stocks []string
-	e = json.Unmarshal(b, &stocks)
+
+	var cfg []Config
+	e = json.Unmarshal(b, &cfg)
 	if e != nil {
 		return nil, e
 	}
-	return stocks, nil
+	return cfg, e
 }
