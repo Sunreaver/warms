203f48d89b6285023aacfa65eaae35773ba09143
TheOne <tanwei.rush@gmail.com>
2016-03-25 16:30:08 +0800

    添加发送邮件成功后的log信息

--- stock_warms.go
+++ stock_warms.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	mail "github.com/sunreaver/goTools/mail"
 	sys "github.com/sunreaver/goTools/system"
@@ -78,6 +79,7 @@ func main() {
 			fmt.Println(e.Error())
 		}
 	}
+	fmt.Println(time.Now().Format("2006-01-02 03:04:05"))
 }
 
 func readFile(fileName string) ([]Config, error) {
