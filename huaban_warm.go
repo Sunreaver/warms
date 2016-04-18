7ff4f59944922fb4aa58b262996722e7546cb519
TheOne <tanwei.rush@gmail.com>
2016-04-18 11:51:56 +0800

    test

--- huaban_warm.go
+++ huaban_warm.go
@@ -186,6 +186,7 @@ func readContent(hb HuaBan) error {
 		r := bytes.NewReader(d)
 		var cf image.Config
 		var e0 error
+
 		if h.File.Type[len("image/"):] == "png" {
 			cf, e0 = png.DecodeConfig(r)
 		} else {
